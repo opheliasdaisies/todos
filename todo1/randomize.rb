a5e0cc27529c6af49bee831750634671190f87f8
Sara Gorecki <sgorecki@gmail.com>
2013-11-12 09:25:21 -0500

    add randomize.rb file

---
+++ todo1/randomize.rb
@@ -0,0 +1,17 @@
+names = ['Keith', 'Nisha', 'Jeremy', 'Sara']
+
+def rand_names(array)
+	index = rand(4)
+	puts array[index]
+end
+
+rand_names(names)
+rand_names(names)
+rand_names(names)
+rand_names(names)
+rand_names(names)
+rand_names(names)
+rand_names(names)
+rand_names(names)
+rand_names(names)
+rand_names(names)
