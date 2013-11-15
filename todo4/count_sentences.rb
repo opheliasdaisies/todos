e59bb8605123ab9d338b1a721077d3a2eba7bd25
Sara Gorecki <sgorecki@gmail.com>
2013-11-15 09:19:17 -0500

    add split method

--- todo4/count_sentences.rb
+++ todo4/count_sentences.rb
@@ -1,8 +1,14 @@
 # Write a method on String called `count_sentences` that returns the number of
 # sentences in the string it is called on
-
+string = "test. one is this. two! three?"
 class String
   def count_sentences
-    # code goes here
+    self.split
   end
 end
+
+puts string.count_sentences
+
+string = ""
+
+puts string.count_sentences
