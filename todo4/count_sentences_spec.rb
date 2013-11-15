e1e24bebf2b07024f1c46e70a87d6d072d3a9059
Sara Gorecki <sgorecki@gmail.com>
2013-11-15 09:21:59 -0500

    revise spec to incorporate sentences more than one word long.

--- todo4/count_sentences_spec.rb
+++ todo4/count_sentences_spec.rb
@@ -3,7 +3,7 @@ require './count_sentences'
 describe String, "#count_sentences" do
 
   it  "should return the number of sentences in a string" do
-    "one. two. three?".count_sentences.should eq(3)
+    "one. two things. three?".count_sentences.should eq(3)
   end
 
   it "should return zero if there are no sentences in a string" do
