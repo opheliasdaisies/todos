264da3b656d70dd6dda3fdf23d675ff7276e9b64
Sara Gorecki <sgorecki@gmail.com>
2013-11-15 09:21:20 -0500

    call length method on array to return number

--- todo4/count_sentences.rb
+++ todo4/count_sentences.rb
@@ -1,14 +1,8 @@
 # Write a method on String called `count_sentences` that returns the number of
 # sentences in the string it is called on
-string = "test. one is this. two! three?"
 class String
   def count_sentences
-    self.split
+    array = self.split
+    array.length
   end
 end
-
-puts string.count_sentences
-
-string = ""
-
-puts string.count_sentences
