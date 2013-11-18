7e30a291b8bdee81ecdaaf27375f39c67fc9d2c6
Sara Gorecki <sgorecki@gmail.com>
2013-11-17 22:53:44 -0500

    make blackjack.rb file and begin constructing game engine

---
+++ games/blackjack.rb
@@ -0,0 +1,46 @@
+#Blackjack!
+
+##Objective
+#Practice contional logic including nested conditionals. 
+#Use methods to keep our code DRY.
+
+##Instructions
+#We are going to build a command line blackjack game.  A player gets dealt two cards which have values between 1-11.  After they get dealt two cards you should show them the total score of their cards and ask them if they want to hit or stay. A player is allowed to "hit" up to two times.  After each hit you should ask if they want to hit or stay and display the total value of their cards. If they don't want to hit, and they are not at 21 they lose.  Your program should tell them they lose and exit.
+
+#Note:  To take input from the person "playing" the game your program will have to use the Ruby method "gets".
+
+##Challenge
+#After successfully completing the previous game (there should be a commit with a working version of this game), alter the game so that a player can "hit" as many times as they want.
+
+def blackjack
+	game_start
+end
+
+def game_start
+	puts "Would you like to begin playing blackjack? Yes or no?"
+	play = gets.chomp
+	if play.downcase == "yes"
+		game
+	elsif play.downcase == "no"
+		puts "You don't want to play? I guess luck just isn't on my side today."
+	else
+		puts "Sorry, I didn't understand that."
+		blackjack
+	end
+end
+
+def game
+	hand = 0
+	deal(hand)
+	deal(hand)
+	puts "You're hand is currently equal to #{hand}."
+end
+
+def deal(hand)
+	card = rand(1..11)
+	puts "You have been dealt a #{card}."
+	hand += card
+end
+
+blackjack
+
