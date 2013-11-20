3ce085e364c0b5a08d1e05186bcf372bf34c2a16
Sara Gorecki <sgorecki@gmail.com>
2013-11-20 09:26:58 -0500

    add a second test to make sure an empty array is returned if an empty array is passed in

--- todo8/listmaker_spec.rb
+++ todo8/listmaker_spec.rb
@@ -4,4 +4,7 @@ describe "#make_list" do
 	it "should return an array, as an array holding a numbered list of strings." do
 		expect(make_list(["this","that","other"])).to eq(["1. this", "2. that", "3. other"])
 	end
+	it "should return an empty array if an empty array is passed in." do
+		expect(make_list([])).to eq([])
+	end
 end
