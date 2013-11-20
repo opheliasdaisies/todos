1f26c897566ff61d3b7b1a3c100acda9ac9b9b5c
Sara Gorecki <sgorecki@gmail.com>
2013-11-20 09:35:40 -0500

    add a test to make sure the method still returns the ordered strings if numbes are passed in

--- todo8/listmaker_spec.rb
+++ todo8/listmaker_spec.rb
@@ -7,4 +7,7 @@ describe "#make_list" do
 	it "should return an empty array if an empty array is passed in." do
 		expect(make_list([])).to eq([])
 	end
+	it "should return an array, as an array holding a numbered list of strings." do
+		expect(make_list([0,1,2])).to eq(["1. 0", "2. 1", "3. 2"])
+	end
 end
