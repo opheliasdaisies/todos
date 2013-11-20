2aa3793bcc3a62df6a547acd4b67745bb0ce3d98
Sara Gorecki <sgorecki@gmail.com>
2013-11-20 09:20:32 -0500

    create test to test make_list method

--- todo8/listmaker_spec.rb
+++ todo8/listmaker_spec.rb
@@ -1,3 +1,7 @@
 require "./listmaker.rb"
 
-describe "#make_list"
+describe "#make_list" do
+	it "should return an array, as an array holding a numbered list of strings." do
+		expect(make_list(["this","that","other"])).to eq(["1. this", "2. that", "3. other"])
+	end
+end
