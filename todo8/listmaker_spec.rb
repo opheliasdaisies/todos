3aef8808ab0f094bda60d57bebd404b122d46e0b
Sara Gorecki <sgorecki@gmail.com>
2013-11-20 09:51:00 -0500

    add a test to test for nested arrays

--- todo8/listmaker_spec.rb
+++ todo8/listmaker_spec.rb
@@ -10,4 +10,7 @@ describe "#make_list" do
 	it "should return an array, as an array holding a numbered list of strings." do
 		expect(make_list([0,1,2])).to eq(["1. 0", "2. 1", "3. 2"])
 	end
+	it "should return nested ordered arrays of strings if a nested array is passed in" do
+		expect(make_list([[4, "pizza"],"oak",["loop"]])).to eq(["1. ['1. 4', '2. pizza']", "2. oak", "3. ['1. loop']"])
+	end
 end
