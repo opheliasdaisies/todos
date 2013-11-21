3b7af3d50f097d84a9ed9c94176856ee9e91e62c
Sara Gorecki <sgorecki@gmail.com>
2013-11-21 10:31:19 -0500

    add test to account for if tickets do not start at no. 1

--- todo8/deli_spec.rb
+++ todo8/deli_spec.rb
@@ -7,6 +7,9 @@ describe Deli, "#take_a_number" do
 	it "should take a customer name, add a number before the name, and add them to the end of the line array" do
 		expect(Deli.new([]).take_a_number("Ashley")).to eq(["1. Ashley"])
 	end
+	it "should take a customer name, add a number before the name, and add them to the end of the line array" do
+		expect(Deli.new(["3. Mark", "4. Jane"]).take_a_number("Ashley")).to eq(["3. Mark", "4. Jane", "5. Ashley"])
+	end
 end
 
 describe Deli, "#now_serving" do
