004b01483b7433cb4252ddca78c23bffda6442b4
Sara Gorecki <sgorecki@gmail.com>
2013-11-22 09:05:01 -0500

    add matcher that will check for same word

--- todo9/anagram.rb
+++ todo9/anagram.rb
@@ -1,6 +1,18 @@
 class Anagram
-	
-	def initialize
+	attr_accessor :word
 
+	def initialize(word)
+		@word = word
 	end
+
+	def match(words_to_check)
+		match_array = []
+		words_to_check.each do |item|
+			if word == item
+				match_array << item
+			end
+		end
+		match_array
+	end
+
 end
