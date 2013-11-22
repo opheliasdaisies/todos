240dd67761019f50e662c3ce179318efdada862b
Sara Gorecki <sgorecki@gmail.com>
2013-11-22 09:32:51 -0500

    expand match method to check for anagrams

--- todo9/anagram.rb
+++ todo9/anagram.rb
@@ -7,12 +7,15 @@ class Anagram
 
 	def match(words_to_check)
 		match_array = []
+		split_word = word.split(//).sort
 		words_to_check.each do |item|
-			if word == item
+			if item.split(//).sort == split_word
 				match_array << item
 			end
 		end
-		match_array
+		match_array.map do |matched_word|
+			matched_word.to_s
+		end
 	end
 
 end
