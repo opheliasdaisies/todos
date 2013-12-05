3b34c175a9e3e4df09ecc10d9618357f782af5c9
Sara Gorecki <sgorecki@gmail.com>
2013-12-05 09:21:23 -0500

    add code to detect equilateral and isosceles triangles

---
+++ todo14/triangle.rb
@@ -0,0 +1,18 @@
+class Triangle
+	attr_reader :side1, :side2, :side3
+
+	def initialize(side1, side2, side3)
+		@side1 = side1
+		@side2 = side2
+		@side3 = side3
+	end
+
+	def kind
+		if side1 == side2 && side2 == side3
+			:equilateral
+		elsif side1 == side2 || side2 == side3 || side3 == side1
+			:isosceles
+		end
+	end
+
+end
