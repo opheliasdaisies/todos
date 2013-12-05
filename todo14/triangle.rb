56babd22c58186173dea1ff4685e45dddea2df07
Sara Gorecki <sgorecki@gmail.com>
2013-12-05 09:35:30 -0500

    add validity check method to throw error if triangle is invalid. need to create TriangleError

--- todo14/triangle.rb
+++ todo14/triangle.rb
@@ -1,6 +1,8 @@
 class Triangle
 	attr_reader :side1, :side2, :side3
 
+	TriangleError = "That triangle is illegal"
+
 	def initialize(side1, side2, side3)
 		@side1 = side1
 		@side2 = side2
@@ -14,6 +16,14 @@ class Triangle
 			:isosceles
 		else
 			:scalene
+		end
+	end
+
+	def validity_check
+		if side1 <= 0 || side2 <= 0 || side3 <= 0
+			raise_error(TriangleError)
+		elsif side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2
+			raise_error(TriangleError)
 		end
 	end
 
