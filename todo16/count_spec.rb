428e6b13c4a224d11a2397cbc4e0a0843ff6ce0e
Sara Gorecki <sgorecki@gmail.com>
2013-12-09 09:24:34 -0500

    add tests for count method

--- todo16/count_spec.rb
+++ todo16/count_spec.rb
@@ -8,3 +8,17 @@
 # end
 
 # count(test) #=> { 'cat' => 1, 'dog' => 1, 'fish' => 2 })
+
+require "./count_items.rb"
+
+describe "#count" do
+	it "should count the elements in an array and return a hash with the keys of elements and values of the number of times they occur." do
+		test = ['cat', 'dog', 'fish', 'fish']
+		expect(count(test)).to eq({ 'cat' => 1, 'dog' => 1, 'fish' => 2 })
+	end	
+
+	it "should count the elements in an array and return a hash with the keys of elements and values of the number of times they occur." do
+		test = ['fish', 'fish', 'fish', 'fish']
+		expect(count(test)).to eq({ 'fish' => 4 })
+	end	
+end
