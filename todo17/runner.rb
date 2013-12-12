526dad6dea1aad321d5004c87a552403ec0bbb98
Sara Gorecki <sgorecki@gmail.com>
2013-12-12 10:23:40 -0500

    remove loop from runner by adding prompt

--- todo17/runner.rb
+++ todo17/runner.rb
@@ -7,10 +7,12 @@ def run
 end
 
 def prompt
+  puts
   puts "Enter a command to continue. Type 'help' for a list of commands."
   command = gets.chomp
   while command.downcase != "exit" do
     run_command(command)
+    prompt
   end
 end
 
@@ -33,8 +35,6 @@ def show_help
   puts("'list' - lists the whole song library")
   puts("or you can enter an artist's name to show that artist's songs")
   puts("or you can enter a song title to play that song!")
-  puts
-  prompt
 end
 
 run
