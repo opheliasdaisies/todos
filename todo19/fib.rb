de5c70d8ebc239c5c8d170ed0aec41dae90ee20d
Sara Gorecki <sgorecki@gmail.com>
2013-12-13 09:17:03 -0500

    refactor code

--- todo19/fib.rb
+++ todo19/fib.rb
@@ -2,10 +2,7 @@
 # create a method that returns the nth number of the fibonacci sequence
 # fib sequence = 0,1,1,2,3,5,8,13 etc, assume the 1st element is 0 (the first number), the 2nd element is 1, the third element is 1
 def fibo_finder(n)
-  return 0 if n==0
-  array = [1,1]
-  n.times do
-    array << array[-1]+array[-2]
-  end
-  array[n-1]
+  array = [0,1]
+  n.times { array << array[-1]+array[-2] }
+  array[n]
 end
