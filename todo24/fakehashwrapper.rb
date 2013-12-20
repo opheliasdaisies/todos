8e5a65be193d7634b7cd7c2e7d77ed00bda8ee8c
Sara Gorecki <sgorecki@gmail.com>
2013-12-20 09:39:40 -0500

    make fake hash

--- todo24/fakehashwrapper.rb
+++ todo24/fakehashwrapper.rb
@@ -1,18 +1,44 @@
-Let's make an object that behaves like a hash today!
+# Let's make an object that behaves like a hash today!
 
-I'd like to be able to find keys in the hash regardless of whether they are strings or symbols.
+# I'd like to be able to find keys in the hash regardless of whether they are strings or symbols.
 
-For example
+# For example
 
-obj = {:blake => "the best"}
-obj["blake"] #=> "the best"
+# obj = {:blake => "the best"}
+# obj["blake"] #=> "the best"
 
-I'd like to be able to set values in our fake hash.
-I'd like to be able to find all values that have keys beginning with the letter a.
-I'd like to be able to get all the keys in the hash in a descending alphabetized array.
+# I'd like to be able to set values in our fake hash.
+# I'd like to be able to find all values that have keys beginning with the letter a.
+# I'd like to be able to get all the keys in the hash in a descending alphabetized array.
 
 class FakeHashWrapper
+  attr_accessor :data
 
+  def initialize(hash)
+    @data = hash
+  end
 
+  def [](key)
+    data[key.to_s]
+    data[key.to_sym]
+  end
+
+  def []=](key, value)
+    data[key] = value
+  end
+
+  def find_key_starting_letter(letter)
+    values = []
+    data.each do |key, value|
+      values << value if key.to_s[0] == letter
+    end
+    values
+  end
+
+  def all_keys
+    keys = []
+    data.each { |key, value| keys << key }
+    keys.sort.reverse
+  end
 
 end
