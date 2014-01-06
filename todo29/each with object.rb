14af94d75ed9e8784241bd3914902fdc8aae0ef5
Sara Gorecki <sgorecki@gmail.com>
2014-01-06 09:09:50 -0500

    add code to return correct values

--- todo29/each with object.rb
+++ todo29/each with object.rb
@@ -3,7 +3,9 @@
 
 # Code:
 def even_sum(arr)
-  arr.each_with_object___
+  arr.each_with_object([]) do |word, a|
+    a << word.reverse if word.length % 2 == 0
+  end
 end
 
-# even_sum(["cat", "dog", "bird", "fish"]) #=> ["drib", "hsif"]
+even_sum(["cat", "dog", "bird", "fish"]) #=> ["drib", "hsif"]
