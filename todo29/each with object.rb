5a43b4425945ca679168e1f94155242334597287
Sara Gorecki <sgorecki@gmail.com>
2014-01-06 09:02:01 -0500

    comment out instructions

--- todo29/each with object.rb
+++ todo29/each with object.rb
@@ -1,7 +1,7 @@
 # Instructions:
 # Each with object works much like inject. Use each_with_object to return an array containing one reversed string for each string that has an even number of characters.
 
-Code:
+# Code:
 def even_sum(arr)
   arr.each_with_object___
 end
