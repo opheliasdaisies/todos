55d0ae9f6a2576f56b372df74bec6fef412cce0a
Sara Gorecki <sgorecki@gmail.com>
2014-01-08 13:51:02 -0500

    add spec to test for multiple arrays of the longest length

--- todo31/spec.rb
+++ todo31/spec.rb
@@ -4,4 +4,10 @@ describe "#longest_palindrome" do
   it "should return the longest palindrome in a given string" do
     expect(longest_palindrome("afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd")).to eq("dhfdkjfffhhfffjkdfhd")
   end
+  it "should return the full string if it is a palindrome" do
+    expect(longest_palindrome("abedkseesjseeskdeba")).to eq("abedkseesjseeskdeba")
+  end
+  it "should return an array of strings if there are multiple palindromes of the longest length" do
+    expect(longest_palindrome("beebplalp")).to eq(["bleeb", "plalp"])
+  end
 end
