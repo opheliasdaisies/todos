cc7302989fcea53f9edf39f5ccf93ec82f99d672
Sara Gorecki <sgorecki@gmail.com>
2014-01-13 09:15:16 -0500

    add rails starting files and scaffold-generated files for posts and comments

---
+++ todo34/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo34::Application.config.secret_token = '81c1f8362c10e54d849618f50b549e79e51131d6658eec6f522c301fbe0a5427641431d33305d8c3d1d0bbd8a318b265a8826f8026b296f4902fc1b35bdc982e'
