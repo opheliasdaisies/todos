a490b5e5a459966df9cdcc7a16080db23535164e
Sara Gorecki <sgorecki@gmail.com>
2014-01-13 09:22:26 -0500

    change routes so as to be nested

--- todo34/config/routes.rb
+++ todo34/config/routes.rb
@@ -1,9 +1,8 @@
 Todo34::Application.routes.draw do
-  resources :comments
-
-
-  resources :posts
-
+  
+  resources :posts do
+    resources :comments
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
