119c98efb02f254bd427de4e6350b146ae2e82a7
Sara Gorecki <sgorecki@gmail.com>
2014-01-14 09:14:41 -0500

    fix spec so that it's testing my_transpose instead of transpose

--- todo35/spec.rb
+++ todo35/spec.rb
@@ -5,9 +5,9 @@ describe "#my_transpose" do
     expect([[1, 2, 3], [:a, :b, :c]].my_transpose).to eq([[1, :a], [2, :b], [3, :c]])
   end
   it "should transpose an array of arrays" do
-    expect([[1,2], [3,4], [5,6]].transpose).to eq([[1, 3, 5], [2, 4, 6]])
+    expect([[1,2], [3,4], [5,6]].my_transpose).to eq([[1, 3, 5], [2, 4, 6]])
   end
   it "should transpose an array of arrays" do
-    expect([].transpose).to eq([])
+    expect([].my_transpose).to eq([])
   end
 end
