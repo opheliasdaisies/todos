2c952b60debf1743a9a78d1e04a0260d09538530
Sara Gorecki <sgorecki@gmail.com>
2014-01-14 09:13:54 -0500

    add starting files for todo35

---
+++ todo35/spec.rb
@@ -0,0 +1,13 @@
+require "./transpose"
+
+describe "#my_transpose" do
+  it "should transpose an array of arrays" do
+    expect([[1, 2, 3], [:a, :b, :c]].my_transpose).to eq([[1, :a], [2, :b], [3, :c]])
+  end
+  it "should transpose an array of arrays" do
+    expect([[1,2], [3,4], [5,6]].transpose).to eq([[1, 3, 5], [2, 4, 6]])
+  end
+  it "should transpose an array of arrays" do
+    expect([].transpose).to eq([])
+  end
+end
