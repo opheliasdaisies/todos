bce0eff8fc64d94b9ae15fda98ff00676a11fcd2
Sara Gorecki <sgorecki@gmail.com>
2014-01-20 08:58:10 -0500

    fix spec so result is a string

--- todo40/spec.rb
+++ todo40/spec.rb
@@ -2,12 +2,12 @@ require "./separate_numbers_with_commas"
 
 describe "#separate_with_comma" do
   it "should separate numbers with a comma each three digits." do
-    expect(separate_with_comma(1)).to eq(1)
+    expect(separate_with_comma(1)).to eq("1")
   end
   it "should separate numbers with a comma each three digits." do
-    expect(separate_with_comma(1000)).to eq(1,000)
+    expect(separate_with_comma(1000)).to eq("1,000")
   end
   it "should separate numbers with a comma each three digits." do
-    expect(separate_with_comma(10000)).to eq(10,000)
+    expect(separate_with_comma(10000)).to eq("10,000")
   end
 end
