a8ffb262395bc2890e52797a1c4d0377cef99825
Sara Gorecki <sgorecki@gmail.com>
2014-01-27 09:13:38 -0500

    fix comments

--- todo42/first_even.js
+++ todo42/first_even.js
@@ -1,6 +1,6 @@
-# prereqs: arrays, methods, conditional logic
-# Write a method that returns the first even element in a array of integers
-# This is an old todo we did in Ruby, please rewrite your answer in javascript
+// prereqs: arrays, methods, conditional logic
+// Write a method that returns the first even element in a array of integers
+// This is an old todo we did in Ruby, please rewrite your answer in javascript
 
 def first_even(items)
   // code goes here
